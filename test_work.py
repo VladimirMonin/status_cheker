58d2f17cac5aba40bd1c263108d66e71baaf3a76
Vladimir Monin <moninvladimir88@gmail.com>
2023-01-31 13:04:47 +0600

    fix: Код рабочий. Но не через вирт. окружение (

--- test_work.py
+++ test_work.py
@@ -16,26 +16,7 @@ if "Ниже форма предзаписи" in text:
 # КОД УВЕДОМЛЕНИЙ
 
 
-# import notify2
-# notify2.init('app name')
-# n = notify2.Notification('title', 'message')
-# n.show()
+import os
 
-# import os
-
-
-# def message_f(title, message):
-#     os.system('notify-send "' + title + '" "' + message + '"')
-#
-#
-# message_f(title, message)
-
-# from tkinter import *
-# import tkinter as tk
-# from tkinter import messagebox
-# root = tk.Tk()
-# root.withdraw()
-# messagebox.showwarning('alert title', 'Bad things happened!')
-
-# import pyautogui as pag
-# pag.alert(text="Bad things happened!", title="alert title")
+command = f'''notify-send'''
+os.system(command)
